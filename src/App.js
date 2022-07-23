b1af89bd1af3f79e2675963b14b50328aa48d31a
Hemlata Pathak <hemlata.pathak@deliveryhero.com>
2022-07-23 21:41:48 +0200

    reeee

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          deploy in production
+          deploy in production chutiya
         </a>
       </header>
     </div>
