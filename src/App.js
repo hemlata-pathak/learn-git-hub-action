cc80e61cbcb3a4d5be4639b900eec37c44c27d73
Hemlata Pathak <hemlata.pathak@deliveryhero.com>
2022-07-24 00:35:48 +0200

    bug fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          deploy in production chutiya
+          I want to fix my bug
         </a>
       </header>
     </div>
