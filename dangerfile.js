366296d442fe262014a74fac374e9fc5e99cae2e
Hemlata Pathak <>
2023-04-25 17:41:05 +0200

    bdbdffdbdfb

--- dangerfile.js
+++ dangerfile.js
@@ -2,3 +2,7 @@ import {message, danger} from "danger"
 
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
+
+message("Additions: \n - " + danger.github.pr.additions)
+
+message("Artifects: \n - " + danger.github.api.actions.getArtifact())
